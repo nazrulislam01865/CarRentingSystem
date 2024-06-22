a72dbefea9704537e91295671009e2774bbed7a3
SamiaSharmin <samiasharmin7654@gmail.com>
2024-06-22 23:19:38 +0600

    hjgjhgjh

--- CarRentingSystem/Program.cs
+++ CarRentingSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarRentingSystem.LoginForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace CarRentingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new loginForm());
+            Application.Run(new LoginDashBoard());
         }
     }
 }
--- CarRentingSystem/Properties/Resources.Designer.cs
+++ CarRentingSystem/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace CarRentingSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Dashboard1 {
+            get {
+                object obj = ResourceManager.GetObject("Dashboard1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ForgetPass {
             get {
                 object obj = ResourceManager.GetObject("ForgetPass", resourceCulture);
